449ec3daf3acc38842423cb566a93638c713db1c
AlejoFerrari463 <alejoferrari463@gmail.com>
2025-02-08 17:02:56 -0300

    agregado de vista home con cards. falta darle diseño

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 
+import fs from "node:fs"
+
 const router = Router()
+
+const rutaProcutos = "./src/data/productos.json"
 
 router.get("/home",(req,res)=>{
 
-    res.render('home',{data: true})
+    const misProductos = fs.readFileSync(rutaProcutos,'utf-8')
+    const data = JSON.parse(misProductos)
+
+    res.render('home',{data})
 
 })
 
